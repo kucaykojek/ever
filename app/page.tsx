248e595a2278ae09905ca54c7c076aad6ba65503
Fajar <kucaykojek@gmail.com>
2023-08-28 03:24:36 +0700

    style: prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
-
 export default function Home() {
-  return (
-    <main>
-      Kucasdasdasdasdasdasd asdasd asdasd
-    </main>
-  )
+  return <main>Kucasdasdasdasdasdasd asdasd asdasd</main>
 }
--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const path = require('path')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
+    includePaths: [path.join(__dirname, 'styles')]
+  }
 }
 
 module.exports = nextConfig
